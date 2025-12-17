c1bf1e37cb4f31b6c9c0458b6ee39f7dde9f06c5
Tarcisio Oliveira <t@rcis.io>
2025-12-17 22:52:09 +0000

    update

--- internal/config/log.go
+++ internal/config/log.go
@@ -5,12 +5,29 @@ type (
 	LogLevel string
 )
 
+const (
+	LogLevelDebug LogLevel = "debug"
+	LogLevelInfo  LogLevel = "info"
+	LogLevelWarn  LogLevel = "warn"
+	LogLevelError LogLevel = "error"
+)
+
 type (
 	// LogFormat represents the encoding style of log records.
 	LogFormat string
 )
 
+const (
+	LogFormatText LogFormat = "text"
+	LogFormatJSON LogFormat = "json"
+)
+
 type (
 	// LogOutput represents the destination stream of log records.
 	LogOutput string
+)
+
+const (
+	LogOutputStdout LogOutput = "stdout"
+	LogOutputStderr LogOutput = "stderr"
 )
