855cd2503b70568fed58be653477ecc50168cdd1
Tarcisio Oliveira <t@rcis.io>
2025-12-17 22:44:07 +0000

    update

--- internal/config/log.go
+++ internal/config/log.go
@@ -1,13 +1,16 @@
 package config
 
 type (
+	// LogLevel represents the severity of log records.
 	LogLevel string
 )
 
 type (
+	// LogFormat represents the encoding style of log records.
 	LogFormat string
 )
 
 type (
+	// LogOutput represents the destination stream of log records.
 	LogOutput string
 )
