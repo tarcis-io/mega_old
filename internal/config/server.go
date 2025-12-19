21751293217e5a189546b15836fb66c62163f159
Tarcisio Oliveira <t@rcis.io>
2025-12-19 01:34:13 +0000

    update

--- internal/config/server.go
+++ internal/config/server.go
@@ -7,22 +7,28 @@ import (
 type (
 	// Server defines the immutable application configuration for the server.
 	Server interface {
-		// Address returns the configured server address.
+		// Address returns the configured TCP address for the server to listen on, in the
+		// form "host:port".
 		Address() string
 
-		// ReadTimeout returns the configured server read timeout.
+		// ReadTimeout returns the configured maximum duration for reading the entire
+		// request, including the body.
 		ReadTimeout() time.Duration
 
-		// ReadHeaderTimeout returns the configured server read header timeout.
+		// ReadHeaderTimeout returns the configured amount of time allowed to read request
+		// headers.
 		ReadHeaderTimeout() time.Duration
 
-		// WriteTimeout returns the configured server write timeout.
+		// WriteTimeout returns the configured maximum duration before timing out writes of
+		// the response.
 		WriteTimeout() time.Duration
 
-		// IdleTimeout returns the configured server idle timeout.
+		// IdleTimeout returns the configured maximum amount of time to wait for the next
+		// request when keep-alives are enabled.
 		IdleTimeout() time.Duration
 
-		// ShutdownTimeout returns the configured server shutdown timeout.
+		// ShutdownTimeout returns the configured grace period allowed for active
+		// connections to close during a graceful shutdown.
 		ShutdownTimeout() time.Duration
 	}
 )
