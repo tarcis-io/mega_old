70fd62e93446ac401350b1421839ee657e8e0c3a
Tarcisio Oliveira <t@rcis.io>
2025-12-19 01:00:45 +0000

    update

--- internal/config/server.go
+++ internal/config/server.go
@@ -1,7 +1,16 @@
 package config
 
+import (
+	"time"
+)
+
 type (
 	Server interface {
 		Address() string
+		ReadTimeout() time.Duration
+		ReadHeaderTimeout() time.Duration
+		WriteTimeout() time.Duration
+		IdleTimeout() time.Duration
+		ShutdownTimeout() time.Duration
 	}
 )
