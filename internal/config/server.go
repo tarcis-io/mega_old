dbf8eb5cc2ae951b4dc615b08265eecd114fcf38
Tarcisio Oliveira <t@rcis.io>
2025-12-19 01:05:50 +0000

    update

--- internal/config/server.go
+++ internal/config/server.go
@@ -5,12 +5,24 @@ import (
 )
 
 type (
+	// Server defines the immutable application configuration for the server.
 	Server interface {
+		// Address returns the configured server address.
 		Address() string
+
+		// ReadTimeout returns the configured server read timeout.
 		ReadTimeout() time.Duration
+
+		// ReadHeaderTimeout returns the configured server read header timeout.
 		ReadHeaderTimeout() time.Duration
+
+		// WriteTimeout returns the configured server write timeout.
 		WriteTimeout() time.Duration
+
+		// IdleTimeout returns the configured server idle timeout.
 		IdleTimeout() time.Duration
+
+		// ShutdownTimeout returns the configured server shutdown timeout.
 		ShutdownTimeout() time.Duration
 	}
 )
