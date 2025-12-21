bec4296dc5e415ed40be838040424b5b0c953465
Tarcisio Oliveira <t@rcis.io>
2025-12-21 23:23:50 +0000

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,8 +2,12 @@
 package config
 
 type (
+	// Config defines the immutable application configuration.
 	Config interface {
+		// Log returns the application configuration for logging.
 		Log() Log
+
+		// Server returns the application configuration for the server.
 		Server() Server
 	}
 )
