beb89f4840ff65e4848d720473a80b40486fa6f1
Tarcisio Oliveira <t@rcis.io>
2025-12-23 20:51:35 +0000

    update

--- internal/config/loader.go
+++ internal/config/loader.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"errors"
+	"fmt"
+)
+
 type (
 	loader struct {
 		errs []error
@@ -8,4 +13,19 @@ type (
 
 func newLoader() *loader {
 	return &loader{}
+}
+
+func (l *loader) addError(err error) {
+	if err == nil {
+		return
+	}
+	l.errs = append(l.errs, err)
+}
+
+func (l *loader) addErrorf(format string, args ...any) {
+	l.addError(fmt.Errorf(format, args...))
+}
+
+func (l *loader) err() error {
+	return errors.Join(l.errs...)
 }
