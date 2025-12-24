abbed242b284c018f3fb9d5b2ce4ce0b267ad31b
Tarcisio Oliveira <t@rcis.io>
2025-12-24 18:48:45 +0000

    update

--- internal/config/loader.go
+++ internal/config/loader.go
@@ -76,6 +76,6 @@ func oneOf[T ~string](l *loader, envKey string, fallback T, allowed ...T) T {
 	for _, a := range allowed {
 		allowedStr = append(allowedStr, string(a))
 	}
-	l.addErrorf("invalid configuration (%s) got=%q allowed=[%v]", envKey, s, allowedStr)
+	l.addErrorf("invalid configuration (%s) got=%q allowed=%v", envKey, s, allowedStr)
 	return fallback
 }
