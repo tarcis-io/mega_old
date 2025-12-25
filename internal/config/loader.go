9dff6ddf86133fad7454a99aa106fbb35f05c685
Tarcisio Oliveira <t@rcis.io>
2025-12-25 15:06:27 +0000

    update

--- internal/config/loader.go
+++ internal/config/loader.go
@@ -31,7 +31,7 @@ func newLoader(lookup lookupFunc) *loader {
 func (l *loader) positiveDuration(envKey string, fallback time.Duration) time.Duration {
 	d := l.duration(envKey, fallback)
 	if d <= 0 {
-		l.addErrorf("invalid configuration: env=%q got=%q err=\"must be positive\"", envKey, d.String())
+		l.addErrorf("invalid configuration: env=%q got=%q err=\"duration must be positive\"", envKey, d.String())
 		return fallback
 	}
 	return d
